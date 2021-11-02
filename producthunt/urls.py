cfa3d6e7eba2a14f359ec2a0b0c6e6a585474842
Tania <taniazuck@gmail.com>
2021-11-02 20:56:41 +0530

    base template added

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products.views.home, name='home'),
+    path('signup/', products.views.signup, name='signup'),
+    path('login/', products.views.signin, name='login'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request,'products/home.html')
+
+def signup(request):
+    return render(request,'products/signup.html')
+
+def signin(request):
+    return render(request,'products/login.html')
